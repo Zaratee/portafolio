2b763578bb60ccde17b76b7aabc3fa3ee1d754f7
Zaratee <39096560+Zaratee@users.noreply.github.com>
2024-06-11 11:16:27 -0600

    Proyect Container
    Scroll
    Animation Skills

--- src/components/ProyectsContainer/ProyectsContainer.js
+++ src/components/ProyectsContainer/ProyectsContainer.js
@@ -3,8 +3,8 @@ import ProyectItem from 'src/components/ProyectItem'
 
 export const ProyectsContainer = () => {
   return (
-    <div className=' flex justify-center items-center bg '>
-        <div className='w-[70%] h-[100%] flex flex-wrap justify-evenly gap-6 py-5  min-h-[90%] overflow-y-scroll'>
+    <div className=' flex justify-center items-center h-[100vh] w-[100vw]'>
+        <div className='w-[70%] h-[100vh]  flex flex-wrap justify-evenly gap-6 py-5  overflow-y-scroll'>
             <ProyectItem/>
             <ProyectItem/>
             <ProyectItem/>
--- src/components/SkillsModal/SkillsModal.js
+++ src/components/SkillsModal/SkillsModal.js
@@ -6,17 +6,18 @@ export const SkillsModal = (props) => {
     const {handleShowSkillsModal, isSkillsActive} = props
     const [showModal, setShowModal] = useState(false)
     useEffect(() => {
+        console.log(isSkillsActive)
         if(!isSkillsActive){
             return
         }
+        console.log('isSkillsActive')
         anime({
             targets: '.modal',
             keyframes:[
                 { translateX: '-150%'  },
                 { translateX: '0%'  },],
             update: function(anim) {
-                console.log(Math.round(anim.progress))
-                Math.round(anim.progress) === 45 && setShowModal(true)              
+                Math.round(anim.progress) === 50 && setShowModal(true)              
             },
             duration: 1000,  
         })
@@ -32,7 +33,7 @@ export const SkillsModal = (props) => {
     }
   return (
     <div className={` w-[100vw] h-[100vh] absolute z-50 backdrop-blur-sm transition-all flex items-center justify-center`}>
-        <div className={`${showModal ? 'visible' : 'hidden'} w-[70%] h-[70%] modal bg-white p-5 relative`}>
+        <div className={`modal ${showModal ? 'visible' : 'hidden'}  w-[70%] h-[70%] shadow-md shadow-black  bg-white p-5 relative`}>
             <div onClick={onCloseModal} className='absolute right-4 top-3 hover:bg-slate-200 cursor-pointer transition-all p-1'><CrossIcon/></div>
             <div className='font-bebas text-black text-6xl'>
                 SKILLS
